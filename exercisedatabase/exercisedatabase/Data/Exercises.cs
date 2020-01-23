436a5bddfbfa35226f742e69c8d585fb3a462adc
Erik Ylavere <erik.ylavere@tptlive.ee>
2020-01-23 09:05:39 +0200

    Update app

--- exercisedatabase/exercisedatabase/Data/Exercises.cs
+++ exercisedatabase/exercisedatabase/Data/Exercises.cs
@@ -20,16 +20,74 @@ namespace exercisedatabase.Data
         public object Next { get; set; }
         public object Previous { get; set; }
         public List<ExercisesDetails> Results { get;set; }
-
-        public object Classifications { get; set; }
+        
+       
 
     }
+    public partial class Muscles
+    {
+        [JsonProperty("target")]
+        public object Target { get; set; }
+
+        [JsonProperty("synergists")]
+        public object Synergists { get; set; }
+
+        [JsonProperty("dynamicStabilizers")]
+        public object DynamicStabilizers { get; set; }
+
+        [JsonProperty("stabilizers")]
+        public object Stabilizers { get; set; }
+
+        [JsonProperty("antogonistStabilizers")]
+        public object AntogonistStabilizers { get; set; }
+    }
+    public partial class Classification
+    {
+        [JsonProperty("utility")]
+        public string Utility { get; set; }
+
+        [JsonProperty("mechanics")]
+        public string Mechanics { get; set; }
+
+        [JsonProperty("force")]
+        public string Force { get; set; }
+
+        [JsonProperty("bearing")]
+        public object Bearing { get; set; }
+
+        [JsonProperty("impact")]
+        public object Impact { get; set; }
+    }
+    public partial class Instructions
+    {
+        [JsonProperty("preparation")]
+        public string Preparation { get; set; }
+
+        [JsonProperty("execution")]
+        public string Execution { get; set; }
+    }
+
     public partial class ExercisesDetails
     {
-        
+
+        [JsonProperty("name")]
         public string Name { get; set; }
-        public string Utility { get; set; }
-        
-        
+
+        [JsonProperty("classification")]
+        public Classification Classification { get; set; }
+
+        [JsonProperty("instructions")]
+        public Instructions Instructions { get; set; }
+
+        [JsonProperty("comments")]
+        public string Comments { get; set; }
+
+        [JsonProperty("muscles")]
+        public Muscles Muscles { get; set; }
+
+        [JsonProperty("gifUrl")]
+        public Uri GifUrl { get; set; }
+
+
     }
 }
